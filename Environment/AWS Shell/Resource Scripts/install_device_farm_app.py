a6a3687b5b7a1a11bbfed2537b14b543022da047
ericrqs <eric.r@quali.com>
2016-07-20 14:41:16 -0500

    add commented-out call to installation driver from install script

--- Environment/AWS Shell/Resource Scripts/install_device_farm_app.py
+++ Environment/AWS Shell/Resource Scripts/install_device_farm_app.py
@@ -19,11 +19,7 @@ api = CloudShellAPISession(host=connectivity['serverAddress'],
                            token_id=connectivity['adminAuthToken'],
                            domain=reservation['domain'])
 
-# installation_result = api.InstallApp(reservationId=resid,
-#                                      resourceName=service,
-#                                      commandName='Install',
-#                                      commandInputs=[],
-#                                      printOutput=True)
+# Temporary implementation bypassing the installation service:
 cp_resource = [x['value']
                for x in resource['appData']['installationService']['attributes']
                if x['name'] == 'AWS EC2'][0]
@@ -32,14 +28,14 @@ deploy_inputs = [InputNameValue(x['name'].lower().replace(' ', '_'), x['value'])
                  for x in resource['appData']['installationService']['attributes']
                  if x['name'] != 'AWS EC2']
 
-
-# with open(r'c:\temp\install_device_farm_app.log', 'a') as f:
-#     f.write('context:' + str(context) + '\n')
-#     f.write('name:' + str(context.resource.name) + '\n')
-#     f.write('aws ec2:' + str(context.resource.attributes["AWS EC2"]) + '\n')
-#     f.write('attributes:' + str(context.resource.attributes) + '\n')
-#     f.write('model:' + str(context.resource.model) + '\n')
-
-#session.WriteMessageToReservationOutput(context.reservation.reservation_id, str(context))
-# return 'success'
 result = api.ExecuteCommand(resid, cp_resource, "Resource", "upload_app", deploy_inputs)
+
+# Version that calls the installation service from this script
+# For this to work, update datamodel.xml on model "AWS Mobile Device Installation":
+# Change to SupportsConcurrentCommands="true"
+
+# installation_result = api.InstallApp(reservationId=resid,
+#                                      resourceName=service,
+#                                      commandName='Install',
+#                                      commandInputs=[],
+#                                      printOutput=True)
